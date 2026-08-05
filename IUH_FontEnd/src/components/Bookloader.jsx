440693c972edc61699ed5aff0241acf0412ebab9
tienminhtran <tientot36@gmail.com>
2026-08-05 15:04:41 +0700

    fix loadding

--- IUH_FontEnd/src/components/Bookloader.jsx
+++ IUH_FontEnd/src/components/Bookloader.jsx
@@ -3,101 +3,83 @@ import logo from "../assets/logo_1.svg";
 
 export default function BookLoader({
   size = 1,
-  colorOuter = "#2b72c4",
-  colorInner = "#4a1575",
+  colorOuter = "#002190",
+  colorInner = "#FFFFFF",
 }) {
-  const px = 80 * size;
+  // Tính toán --size dựa trên prop size truyền vào
+  const baseSize = 1 * size;
 
   return (
     <>
       <style>{`
-        .book-loader{
-          position:relative;
-          display:inline-flex;
-          align-items:center;
-          justify-content:center;
+        .loader {
+          --color-1: ${colorOuter};
+          --color-2: ${colorInner};
+          --size: ${baseSize}px;
+
+          width: calc(48 * var(--size));
+          height: calc(48 * var(--size));
+          border: calc(3 * var(--size)) solid var(--color-1);
+          border-radius: 50%;
+          display: inline-flex;
+          align-items: center;
+          justify-content: center;
+          position: relative;
+          box-sizing: border-box;
+          animation: rotation 1s linear infinite;
         }
 
-        .book-loader svg{
-          width:100%;
-          height:100%;
+        .loader::after {
+          content: '';
+          box-sizing: border-box;
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+          width: calc(40 * var(--size));
+          height: calc(40 * var(--size));
+          border-radius: 50%;
+          border: calc(3 * var(--size)) solid;
+          border-color: var(--color-2) transparent;
         }
 
-        .book-loader-logo{
-          position:absolute;
-          width:38%;
-          height:38%;
-          object-fit:contain;
-          z-index:10;
-          user-select:none;
-          pointer-events:none;
+        .book-loader-logo {
+          position: absolute;
+          width: 45%;
+          height: 45%;
+          object-fit: contain;
+          z-index: 10;
+          user-select: none;
+          pointer-events: none;
+          /* Hủy xoay cho logo nếu muốn logo đứng yên */
+          animation: counter-rotation 1s linear infinite;
         }
 
-        .outer-ring{
-          animation:spinClockwise 2s linear infinite;
-          transform-origin:center;
-        }
-
-        .inner-ring{
-          animation:spinCounterClockwise 1.5s linear infinite;
-          transform-origin:center;
-        }
-
-        @keyframes spinClockwise{
-          from{
-            transform:rotate(0deg);
+        @keyframes rotation {
+          0% {
+            transform: rotate(0deg);
           }
-          to{
-            transform:rotate(360deg);
+          100% {
+            transform: rotate(360deg);
           }
         }
 
-        @keyframes spinCounterClockwise{
-          from{
-            transform:rotate(0deg);
+        @keyframes counter-rotation {
+          0% {
+            transform: rotate(0deg);
           }
-          to{
-            transform:rotate(-360deg);
+          100% {
+            transform: rotate(-360deg);
           }
         }
       `}</style>
 
-      <div
-        className="book-loader"
-        style={{
-          width: px,
-          height: px,
-        }}
-      >
-        <svg viewBox="0 0 100 100">
-          {/* Vòng ngoài */}
-          <g className="outer-ring">
-            <path
-              d="M20 50 A30 30 0 0 1 80 50"
-              fill="none"
-              stroke={colorOuter}
-              strokeWidth="7"
-              strokeLinecap="round"
-            />
-          </g>
-
-          {/* Vòng trong */}
-          <g className="inner-ring">
-            <path
-              d="M50 28 A22 22 0 0 1 50 72"
-              fill="none"
-              stroke={colorInner}
-              strokeWidth="7"
-              strokeLinecap="round"
-            />
-          </g>
-        </svg>
-
-        {/* Logo giữa */}
+      <div className="loader">
+        {/* Logo giữ ở trung tâm */}
         <img
           src={logo}
           alt="Logo"
-          className="book-loader-logo size-logo"
+          className="book-loader-logo"
           draggable={false}
         />
       </div>
